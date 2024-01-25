3329b093ea309595fecb53e5e2e2cd6c0bb73bdd
xwasoux <oxwasouxo@gmail.com>
2024-01-25 06:24:32 +0000

    feat: upgrade init script

--- apt.sh
+++ apt.sh
@@ -1,4 +1,18 @@
 sudo apt update -y
-sudo apt -y install tig \
+sudo apt -y install \
+    tig \
     htop \
-    tmux
+    tmux \
+    vim \
+    tree
+
+git config --global --add safe.directory /workspace/.git
+echo "Enter your git account name: "
+read user
+echo "Enter your git account email: "
+read email
+git config --global user.name $user
+git config --global user.email $email
+
+git config core.fileMode false
+sudo chmod -R 777 .
